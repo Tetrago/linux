68330a29feea1dd45b0c1b1f3b1d0d514c68b760
James <jamestgeiss@gmail.com>
2021-08-24 16:37:27 -0400

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -71,7 +71,7 @@ pacman_install ()
   pacman -Sq --noconfirm --needed $2 &> /dev/null
 }
 
-packages=( neovim git base-devel xorg-server make cmake emacs bspwm fish picom nitrogen lightdm alacritty xterm dmenu dunst tldr man exa procs bat ripgrep fd neofetch lxsession network-manager-applet pcmanfm-gtk3 lxappearance feh xfce4-power-manager ufw gvfs alsa-utils playerctl pulseaudio pulseaudio-alsa pavucontrol zathura zathura-pdf-mupdf thefuck nnn kmon lazygit zoxide )
+packages=( neovim git base-devel xorg-server make cmake emacs bspwm sxhkd fish picom nitrogen lightdm alacritty xterm dmenu dunst tldr man exa procs bat ripgrep fd neofetch lxsession network-manager-applet pcmanfm-gtk3 lxappearance feh xfce4-power-manager ufw gvfs alsa-utils playerctl pulseaudio pulseaudio-alsa pavucontrol zathura zathura-pdf-mupdf thefuck nnn kmon lazygit zoxide )
 
 for i in "${packages[@]}"
 do
@@ -217,6 +217,9 @@ log_step 0 "Remaining settings..."
 
 log_list 1 "Enabling UFW"
 systemctl enable ufw &> /dev/null
+
+log_list 1 "Making vim symbolic link..."
+ln -s /usr/bin/nvim /usr/local/bin/vim
 
 # --- Finializing setup process -------------------------------------------------------------------
 

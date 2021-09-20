90742524023c61b981acc5cc6747eb2bba1374c2
James <jamestgeiss@gmail.com>
2021-09-20 16:20:41 -0400

    Update setup.sh

--- arch/setup.sh
+++ arch/setup.sh
@@ -123,6 +123,7 @@ declare -a aur_packages=(
     "nerd-fonts-complete"
     "faba-icon-theme" # Needed by dunst volume controller in dwm.
     "consolas-font"
+    "ttf-ms-fonts"
     "archlinux-wallpaper"
     "google-chrome"
     "glow"
